da1b37885b62b6efeaf529957fa03c2319914963
Solyarka <97371336+Solyarka69@users.noreply.github.com>
2022-05-21 10:57:31 +0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -9,15 +9,12 @@ class MyApplication
 {
     public static void Main(String[] args)
     {
-        CharacteristicsRobot botyara = new CharacteristicsRobot("BOTYARA",300,new double[] {1.2,3.4,5.1}); 
-            botyara.printCharacteristics();
-
-        CharacteristicsRobot botyara2 = new CharacteristicsRobot();
-            botyara2.getCharacteristics("BOTYARA2",200,new double[] {2.1,7.8,9.9}); 
-            botyara2.printCharacteristics();
-
+        CarFeatures FirstCar = new CarFeatures("BMW", "black edition", 350.3, 2022);
+        FirstCar.OutputFeatures();
         
-        
+        CarFeatures SecondCar = new CarFeatures("Audi", "white edition", 433.8, 2021);
+        SecondCar.OutputFeatures();
+         
 
     }
 }
